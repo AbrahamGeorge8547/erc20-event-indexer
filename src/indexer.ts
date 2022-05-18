ed36dcbb8f715fea5e845e6c45837acd35506e44
Abraham George <abrahamgeorge8547@gmail.com>
2022-05-18 14:46:05 +0530

    refactoring indexer

--- src/indexer.ts
+++ src/indexer.ts
@@ -1,41 +1,21 @@
 import { ethers } from 'ethers';
-import tokenMetaRepo from './repository/tokenMeta.repo';
+import tokenMetaService from './services/meta.service';
 import Logger from './logger';
 import ItokenMeta from './interfaces/tokenMeta.interface';
 import tokenService from './services/token.service';
-import {INFURA_ID, ERC20_ABI} from './constants'; 
-
+import {INFURA_ID, ERC20_ABI } from './constants'; 
+const logger = new Logger('Indexer');
 const indexer = async (tokenAddress: string) => {
-  const logger = new Logger('Indexer');
-
+  logger.info("Initilizing provider...");
   const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
-
-
-  logger.info('Testing');
   const contract = new ethers.Contract(tokenAddress, ERC20_ABI, provider);
   const block = await provider.getBlockNumber();
-  const check = await tokenMetaRepo.exists(tokenAddress);
-  if (!check) {
-    const promises = [contract.name(), contract.decimals(), contract.symbol()]
-    const result = await Promise.allSettled(promises);
-    let data:Partial<ItokenMeta> = {}
-    data.status = "CREATED";
-    data.tokenAddress = tokenAddress;
-    if(result[0].status === 'fulfilled') {
-      data['name'] = result[0].value;
-    }
-    if(result[1].status === 'fulfilled') {
-      data['decimal'] = ethers.BigNumber.from(result[1].value).toNumber();
-    }
-    if(result[2].status === 'fulfilled') {
-      data['symbol'] = result[2].value;
-    }
-
-    await tokenMetaRepo.create(data);
-  }
   const transferFilter = contract.filters.Transfer();
+  const firstBlock = await tokenMetaService.checkOrCreate(tokenAddress, contract);
+  console.log(firstBlock);
   await tokenService.fetch(tokenAddress);
-  // const transferDetails = await contract.queryFilter(transferFilter, block - 100, block);
+  // const transferDetails = await contract.queryFilter(transferFilter, 50000, 100000);
+  // console.log(transferDetails)
   // console.log('******************************************8',transferDetails)
   // for (let i=0; i<transferDetails.length; i++) {
   //   const from = transferDetails[i].args?.from;
