968115e8a525473c4c6e00980035bb66d0348445
Abraham George <abrahamgeorge8547@gmail.com>
2022-05-20 19:39:36 +0530

    changing to command line args

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 // import os from 'os';
-import connector from './models/databaseConnector';
-
-import indexer from './indexer';
+import connector from "./models/databaseConnector";
+import Logger from "./logger";
+import indexer from "./indexer";
 const app = async () => {
   // console.log(process.argv[2], os.cpus())
+  const address = process.argv[2];
+  if (!address) {
+    Logger.general("Token Address not found. Exiting ...");
+  }
   await connector();
-  await indexer('0xdAC17F958D2ee523a2206206994597C13D831ec7');
+  await indexer(address);
 };
 
 app();
